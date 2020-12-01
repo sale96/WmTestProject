5d28e229ca6a2a52c3ca6924030e43bd31311434
Aleksandar <aca801@gmail.com>
2020-12-02 00:40:50 +0100

    Add Category, Supplier, Manufacturer fields to Product entity

--- WmTestProject.Domain/Entities/Product.cs
+++ WmTestProject.Domain/Entities/Product.cs
@@ -9,5 +9,12 @@ namespace WmTestProject.Domain.Entities
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
+
+        public int ManufacturerId { get; set; }
+        public Manufacturer Manufacturer { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public int SupplierId { get; set; }
+        public Supplier Supplier { get; set; }
     }
 }
