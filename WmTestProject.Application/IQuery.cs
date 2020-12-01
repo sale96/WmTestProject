2d77c52028ec1746895ad39c726f0f1b0b42b103
Aleksandar <aca801@gmail.com>
2020-12-01 23:59:02 +0100

    Add UseCase interfaces

---
+++ WmTestProject.Application/IQuery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WmTestProject.Application
+{
+    public interface IQuery<TSearch, TResult> : IUseCase
+    {
+        TResult Execute(TSearch search);
+    }
+}
---
+++ WmTestProject.Application/IUseCase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WmTestProject.Application
+{
+    public interface IUseCase
+    {
+        string Name { get; }
+    }
+}
