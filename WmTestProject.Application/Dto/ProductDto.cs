62acad19c785d64b6e67be8c02b7169e1b222bc8
Aleksandar <aca801@gmail.com>
2020-12-02 00:48:58 +0100

    Add basic ProductDto

---
+++ WmTestProject.Application/Dto/ProductDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WmTestProject.Application.Dto
+{
+    public class ProductDto : BaseDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Manufacturer { get; set; }
+        public string Supplier { get; set; }
+    }
+}
