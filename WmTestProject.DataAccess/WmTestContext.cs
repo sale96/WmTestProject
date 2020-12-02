364d4d04cc943480b4a02d8e4f46fa0f9fb618b0
Aleksandar <aca801@gmail.com>
2020-12-02 02:29:05 +0100

    Add connection string to DB context

--- WmTestProject.DataAccess/WmTestContext.cs
+++ WmTestProject.DataAccess/WmTestContext.cs
@@ -2,10 +2,22 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using WmTestProject.Domain.Entities;
 
 namespace WmTestProject.DataAccess
 {
     public class WmTestContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=WmTest;Integrated Security=True;");
+
+            base.OnConfiguring(optionBuilder);
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
