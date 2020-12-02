5201432651dba39295d6de1fedc7a853119d313c
Aleksandar <aca801@gmail.com>
2020-12-02 12:50:20 +0100

    Add values to entity CreatedAt and UpdatedAt automaticaly depending on entity state

--- WmTestProject.DataAccess/WmTestContext.cs
+++ WmTestProject.DataAccess/WmTestContext.cs
@@ -21,6 +21,28 @@ namespace WmTestProject.DataAccess
             modelBuilder.ApplyConfiguration(new ProductConfiguration());
         }
 
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries())
+            {
+                if (entry.Entity is BaseEntity e)
+                {
+                    switch (entry.State)
+                    {
+                        case EntityState.Modified:
+                            e.CreatedAt = DateTime.Now;
+                            e.UpdatedAt = null;
+                            break;
+                        case EntityState.Added:
+                            e.UpdatedAt = DateTime.Now;
+                            break;
+                    }
+                }
+            }
+
+            return base.SaveChanges();
+        }
+
         public DbSet<Category> Categories { get; set; }
         public DbSet<Supplier> Suppliers { get; set; }
         public DbSet<Manufacturer> Manufacturers { get; set; }
--- WmTestProject.Domain/Entities/BaseEntity.cs
+++ WmTestProject.Domain/Entities/BaseEntity.cs
@@ -8,6 +8,6 @@ namespace WmTestProject.Domain.Entities
     {
         public int Id { get; set; }
         public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
     }
 }
