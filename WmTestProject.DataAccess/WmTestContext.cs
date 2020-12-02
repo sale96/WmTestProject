8208837b10e1ff100af30fb3a7016a02c2526109
Aleksandar <aca801@gmail.com>
2020-12-02 01:27:49 +0100

    Add empty dbcontext

---
+++ WmTestProject.DataAccess/WmTestContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WmTestProject.DataAccess
+{
+    public class WmTestContext : DbContext
+    {
+    }
+}
