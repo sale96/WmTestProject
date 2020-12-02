1daf0bd7dbd3ce8e1f66ea478e068f2faee11c3f
Aleksandar <aca801@gmail.com>
2020-12-02 16:29:34 +0100

    Register GetProductsService to DI container

---
+++ WmTestProject.Web/Services/QueryServices.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WmTestProject.Application.Queries.Product;
+using WmTestProject.Implementation.Queries.Product;
+
+namespace WmTestProject.Web.Services
+{
+    public static class QueryServices
+    {
+        public static IServiceCollection RegisterQueries(this IServiceCollection services)
+        {
+            services.AddTransient<IGetProductsQuery, GetProductsQuery>();
+
+            return services;
+        }
+    }
+}
--- WmTestProject.Web/Startup.cs
+++ WmTestProject.Web/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using WmTestProject.DataAccess;
+using WmTestProject.Web.Services;
 
 namespace WmTestProject.Web
 {
@@ -31,9 +32,10 @@ namespace WmTestProject.Web
                     Configuration.GetConnectionString("DefaultConnection"),
                     b => b.MigrationsAssembly(typeof(WmTestContext).Assembly.FullName));
             });
+
+            services.RegisterQueries();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
