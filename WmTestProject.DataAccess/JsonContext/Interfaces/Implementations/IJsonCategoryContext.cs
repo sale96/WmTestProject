a5b886601f9dde31950836ca22b9949e2fcab2f0
Aleksandar <aca801@gmail.com>
2020-12-03 22:30:36 +0100

    Add JsonCategoryContext

---
+++ WmTestProject.DataAccess/JsonContext/Interfaces/Implementations/IJsonCategoryContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WmTestProject.Application.Dto;
+
+namespace WmTestProject.DataAccess.JsonContext.Interfaces.Implementations
+{
+    public interface IJsonCategoryContext : IJsonContextRead<IEnumerable<CategoryDto>>, IJsonContextWrite<CategoryDto>
+    {
+    }
+}
---
+++ WmTestProject.DataAccess/JsonContext/JsonCategoryContext.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using WmTestProject.Application.Dto;
+using WmTestProject.DataAccess.JsonContext.Interfaces;
+using WmTestProject.DataAccess.JsonContext.Interfaces.Implementations;
+
+namespace WmTestProject.DataAccess.JsonContext
+{
+    public class JsonCategoryContext : JsonContextBase, IJsonCategoryContext
+    {
+        public IEnumerable<CategoryDto> Read()
+        {
+            return base.Read().Categories;
+        }
+
+        public void Write(CategoryDto entity)
+        {
+            var categories = Read().ToList();
+
+            var exist = categories.FirstOrDefault(x => x.Id == entity.Id) == null;
+
+            if (!exist)
+            {
+                categories.Add(entity);
+            }
+
+            var data = base.Read();
+            data.Categories = categories;
+
+            var fileData = JsonConvert.SerializeObject(data);
+            File.WriteAllText(file, fileData);
+        }
+    }
+}
