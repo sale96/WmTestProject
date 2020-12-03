bb68b040f73e56dfa46afd114226577602d0a5a9
Aleksandar <aca801@gmail.com>
2020-12-03 18:28:41 +0100

    Add Add Products commands

---
+++ WmTestProject.Application/Commands/Product/IAddProductCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WmTestProject.Application.Dto;
+
+namespace WmTestProject.Application.Commands.Product
+{
+    public interface IAddProductCommand : ICommand<ProductDto>
+    {
+    }
+}
---
+++ WmTestProject.Implementation/Commands/ProductCommands/AddProductCommand.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WmTestProject.Application.Commands.Product;
+using WmTestProject.Application.Dto;
+using WmTestProject.DataAccess;
+
+namespace WmTestProject.Implementation.Commands.ProductCommands
+{
+    public class AddProductCommand : IAddProductCommand
+    {
+        private readonly WmTestContext _context;
+        private readonly IMapper _mapper;
+
+        public AddProductCommand(
+            WmTestContext context,
+            IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+
+        }
+
+        public string Name => typeof(AddProductCommand).Name;
+
+        public void Execute(ProductDto request)
+        {
+        }
+    }
+}
