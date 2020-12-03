a08c9b745687ab56bffe895a80b6d61adc6320b1
Aleksandar <aca801@gmail.com>
2020-12-03 22:45:00 +0100

    Add JSON supplier context

---
+++ WmTestProject.DataAccess/JsonContext/Interfaces/Implementations/IJsonSupplierContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WmTestProject.Application.Dto;
+
+namespace WmTestProject.DataAccess.JsonContext.Interfaces.Implementations
+{
+    public interface IJsonSupplierContext : IJsonContextRead<IEnumerable<SupplierDto>>, IJsonContextWrite<SupplierDto>
+    {
+    }
+}
---
+++ WmTestProject.DataAccess/JsonContext/JsonSupplierContext.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using WmTestProject.Application.Dto;
+using WmTestProject.DataAccess.JsonContext.Interfaces;
+using WmTestProject.DataAccess.JsonContext.Interfaces.Implementations;
+
+namespace WmTestProject.DataAccess.JsonContext
+{
+    public class JsonSupplierContext : JsonContextBase, IJsonSupplierContext
+    {
+        public IEnumerable<SupplierDto> Read()
+        {
+            return base.Read().Suppliers;
+        }
+
+        public void Write(SupplierDto entity)
+        {
+            var supplier = Read().ToList();
+
+            var exist = supplier.FirstOrDefault(x => x.Id == entity.Id) == null;
+
+            if (!exist)
+            {
+                supplier.Add(entity);
+            }
+
+            var data = base.Read();
+            data.Suppliers = supplier;
+
+            var fileData = JsonConvert.SerializeObject(data);
+            File.WriteAllText(file, fileData);
+        }
+    }
+}
