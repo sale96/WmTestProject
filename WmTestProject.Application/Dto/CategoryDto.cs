65fe2c8b6a2947d7f8f34f95b415ac3bc269036e
Aleksandar <aca801@gmail.com>
2020-12-03 18:27:27 +0100

    Add Dtos for Category, Manufacturer, Supplier

---
+++ WmTestProject.Application/Dto/CategoryDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WmTestProject.Application.Dto
+{
+    public class CategoryDto : BaseDto
+    {
+        public int Name { get; set; }
+    }
+}
