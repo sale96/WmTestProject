7be0cd19157933c0d215eca358292f49c7663f4c
Aleksandar <aca801@gmail.com>
2020-12-03 22:36:29 +0100

    Add Json Manufacturer context

---
+++ WmTestProject.DataAccess/JsonContext/Interfaces/Implementations/IJsonManufacturerContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WmTestProject.Application.Dto;
+
+namespace WmTestProject.DataAccess.JsonContext.Interfaces.Implementations
+{
+    public interface IJsonManufacturerContext : IJsonContextRead<IEnumerable<ManufacturerDto>>, IJsonContextWrite<ManufacturerDto>
+    {
+    }
+}
---
+++ WmTestProject.DataAccess/JsonContext/JsonManufacturerContext.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using WmTestProject.Application.Dto;
+using WmTestProject.DataAccess.JsonContext.Interfaces.Implementations;
+
+namespace WmTestProject.DataAccess.JsonContext
+{
+    public class JsonManufacturerContext : JsonContextBase, IJsonManufacturerContext
+    {
+        public IEnumerable<ManufacturerDto> Read()
+        {
+            return base.Read().Manufactures;
+        }
+
+        public void Write(ManufacturerDto entity)
+        {
+            var manufacturers = Read().ToList();
+
+            var exist = manufacturers.FirstOrDefault(x => x.Id == entity.Id) == null;
+
+            if (!exist)
+            {
+                manufacturers.Add(entity);
+            }
+
+            var data = base.Read();
+            data.Manufactures = manufacturers;
+
+            var fileData = JsonConvert.SerializeObject(data);
+            File.WriteAllText(file, fileData);
+        }
+    }
+}
