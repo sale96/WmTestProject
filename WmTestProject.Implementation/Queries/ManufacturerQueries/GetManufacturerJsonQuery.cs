7186880ecd7a3d3e6768af3029777a293b022d1e
Aleksandar <aca801@gmail.com>
2020-12-04 07:55:46 +0100

    Add Manufacturer json query

---
+++ WmTestProject.Implementation/Queries/ManufacturerQueries/GetManufacturerJsonQuery.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WmTestProject.Application.Dto;
+using WmTestProject.Application.Queries.Manufacturer;
+using WmTestProject.DataAccess.JsonContext.Interfaces.Implementations;
+
+namespace WmTestProject.Implementation.Queries.ManufacturerQueries
+{
+    public class GetManufacturerJsonQuery : IGetManufacturersQuery
+    {
+        private readonly IJsonManufacturerContext _context;
+
+        public GetManufacturerJsonQuery(IJsonManufacturerContext context)
+        {
+            _context = context;
+        }
+
+        public string Name => typeof(GetManufacturerJsonQuery).Name;
+
+        public IReadOnlyList<ManufacturerDto> Execute(string search)
+        {
+            return _context.Read().ToList();
+        }
+    }
+}
