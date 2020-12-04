fea5fa7c03a7b88e59691771c733774357fd97df
Aleksandar <aca801@gmail.com>
2020-12-04 07:52:54 +0100

    Add Categories Json query

---
+++ WmTestProject.Implementation/Queries/CategoryQueries/GetCategoriesJsonQuery.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WmTestProject.Application.Dto;
+using WmTestProject.Application.Queries.Categories;
+using WmTestProject.DataAccess.JsonContext.Interfaces.Implementations;
+
+namespace WmTestProject.Implementation.Queries.CategoryQueries
+{
+    public class GetCategoriesJsonQuery : IGetCategoriesQuery
+    {
+        private readonly IJsonCategoryContext _categoryContext;
+
+        public GetCategoriesJsonQuery(IJsonCategoryContext categoryContext)
+        {
+            _categoryContext = categoryContext;
+        }
+
+        public string Name => typeof(GetCategoriesJsonQuery).Name;
+
+        public IReadOnlyList<CategoryDto> Execute(string search)
+        {
+            return _categoryContext.Read().ToList();
+        }
+    }
+}
