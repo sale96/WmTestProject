4ac34f6fe58915d317585e41a9bc8bddbe160fe2
Aleksandar <aca801@gmail.com>
2020-12-04 01:49:14 +0100

    Add String utilities class

---
+++ WmTestProject.Implementation/Utilities/StringUtilities.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WmTestProject.Implementation.Utilities
+{
+    public static class StringUtilities
+    {
+        public static bool IsTotalyEmpty(this string providedString)
+            => string.IsNullOrEmpty(providedString) || string.IsNullOrWhiteSpace(providedString);
+    }
+}
