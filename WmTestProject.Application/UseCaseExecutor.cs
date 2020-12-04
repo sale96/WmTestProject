2a271d676d12143c744d5da16538377da161068d
Aleksandar <aca801@gmail.com>
2020-12-04 17:50:50 +0100

    Add UseCase executor class for executing queries and commands

---
+++ WmTestProject.Application/UseCaseExecutor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WmTestProject.Application
+{
+    public class UseCaseExecutor
+    {
+        public TResult ExecuteQuery<TSearch, TResult>(
+            IQuery<TSearch, TResult> query,
+            TSearch search)
+        {
+            return query.Execute(search);
+        }
+
+        public void ExecuteCommand<TRequest>(
+            ICommand<TRequest> command,
+            TRequest request)
+        {
+            command.Execute(request);
+        }
+    }
+}
