3558c0d971c4492c9ec3864bb123e3a566b927a8
Aleksandar <aca801@gmail.com>
2020-12-04 07:59:34 +0100

    Add Suppliers json query

---
+++ WmTestProject.Implementation/Queries/SupplierQueries/GetSuppliersJsonQuery.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WmTestProject.Application.Dto;
+using WmTestProject.Application.Queries.Supplier;
+using WmTestProject.DataAccess.JsonContext.Interfaces.Implementations;
+
+namespace WmTestProject.Implementation.Queries.SupplierQueries
+{
+    public class GetSuppliersJsonQuery : IGetSuppliersQuery
+    {
+        private readonly IJsonSupplierContext _context;
+
+        public GetSuppliersJsonQuery(IJsonSupplierContext context)
+        {
+            _context = context;
+        }
+
+        public string Name => typeof(GetSuppliersJsonQuery).Name;
+
+        public IReadOnlyList<SupplierDto> Execute(string search)
+        {
+            return _context.Read().ToList();
+        }
+    }
+}
