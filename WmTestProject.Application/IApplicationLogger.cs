139180a82b5890f0e25a087d9a088d92fc0d5764
Aleksandar <aca801@gmail.com>
2020-12-04 18:03:41 +0100

    Add basic logging implementation

---
+++ WmTestProject.Application/IApplicationLogger.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WmTestProject.Application
+{
+    public interface IApplicationLogger
+    {
+        void Log(string name, object data);
+    }
+}
---
+++ WmTestProject.Implementation/Loggers/ConsoleLogger.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WmTestProject.Application;
+
+namespace WmTestProject.Implementation.Loggers
+{
+    public class ConsoleLogger : IApplicationLogger
+    {
+        public void Log(string name, object data)
+        {
+            var msg = $"{name} is executed with the data {JsonConvert.SerializeObject(data)}";
+            Console.WriteLine(msg);
+        }
+    }
+}
