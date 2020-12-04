907dee287ead664b7a6d4ce7c81bd85afd0e2387
Aleksandar <aca801@gmail.com>
2020-12-04 14:21:09 +0100

    Fix generic parameter misstake

--- WmTestProject.Application/Queries/Product/IUpdateProductCommand.cs
+++ WmTestProject.Application/Queries/Product/IUpdateProductCommand.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WmTestProject.Application.Dto;
 
 namespace WmTestProject.Application.Queries.Product
 {
-    public interface IUpdateProductCommand : ICommand<int>
+    public interface IUpdateProductCommand : ICommand<ProductDto>
     {
     }
 }
